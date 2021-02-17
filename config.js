a594581a33ef74c77a90ba522d8d3807cf6e9848
MelvynnHadia <69381490+MelvynnHadia@users.noreply.github.com>
2021-02-17 21:50:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyAUFabdmS0cjHkN1UQ5pNJzZpaN14ExphQ",
+    authDomain: "bartersystem-d857b.firebaseapp.com",
+    projectId: "bartersystem-d857b",
+    storageBucket: "bartersystem-d857b.appspot.com",
+    messagingSenderId: "315812897764",
+    appId: "1:315812897764:web:8577997a78a526e3f59df1"
+  };
+  firebase.initializeApp(firebaseConfig);
